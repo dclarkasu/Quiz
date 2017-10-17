048203010c3eb0f625ab122fbdaf0164b3084a85
dclarkasu <dclark205@gmail.com>
2017-10-17 14:41:29 -0600

    initial commit

---
+++ quizdb.sql
@@ -0,0 +1,22 @@
+
+DROP DATABASE IF EXISTS `quizdb`;
+
+CREATE DATABASE IF NOT EXISTS `quizdb` DEFAULT CHARACTER SET utf8;
+
+USE `quizdb`;
+
+DROP TABLE IF EXISTS `Quiz`;
+
+CREATE TABLE IF NOT EXISTS `Quiz` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+START TRANSACTION;
+
+USE `quizdb`;
+INSERT INTO `Quiz` (`name`)
+VALUES ('Quiz #1');
+
+COMMIT;
